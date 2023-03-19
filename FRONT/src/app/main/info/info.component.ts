5251940fdfc3f7091db2193580ebcd33af664f50
Sergio Sabando <sergsaba@espol.edu.ec>
2023-03-18 22:55:47 -0500

    Info functionality

--- FRONT/src/app/main/info/info.component.ts
+++ FRONT/src/app/main/info/info.component.ts
@@ -1,3 +1,4 @@
+import { InfoModel } from './../home/model/info.interface';
 import { ProjectModel } from './../home/model/project.interface';
 import { Component, Output, EventEmitter, Input } from '@angular/core';
 
@@ -10,6 +11,33 @@ export class InfoComponent {
   @Output() onCancel:EventEmitter<void>=new EventEmitter();
   @Output() onSave:EventEmitter<ProjectModel>=new EventEmitter();
   @Input() project!:ProjectModel;
+  valorX:string="";
+  input:string="";
+  output:string="";
+  valorY:string="";
+  add(){
+    this.project.infoList.push({
+      valueX:this.valorX,
+      valueY:this.valorY,
+      id:0,
+      projectId:this.project.id
+    });
+  }
+  eliminar(item:InfoModel){
+    var indice=this.project.infoList.indexOf(item);
+    if(indice>=0){
+      this.project.infoList.splice(indice,1);
+    }
+  }
+  reemplazar() {
+    let out = this.input;
+    this.project.infoList.forEach(el => {
+      const searchEscaped = el.valueX.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+      const regex = new RegExp(searchEscaped, 'g');
+      out = out.replace(regex, el.valueY);
+    });
+    this.output = out;
+  }
   cancel(){
     this.onCancel.emit();
   }
