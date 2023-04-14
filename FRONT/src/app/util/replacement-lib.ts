3bba09945b200fc4c6d3fc45845630eabfc70cb6
Sergio Sabando <sergio.sabando@compsesa.com.ec>
2023-04-14 17:59:23 -0500

    100%

--- FRONT/src/app/util/replacement-lib.ts
+++ FRONT/src/app/util/replacement-lib.ts
@@ -8,7 +8,8 @@ export class ReplacementLib{
                 project.infoList.forEach(el => {
                 copiedText = this.replaceText(copiedText,el.valueX, el.valueY);
                 });
-                await ClipboardUtils.setText(copiedText).then((message) => {
+                //here
+                await ClipboardUtils.setText(this.replaceAddOutParameter(this.replaceOracleDbType(this.replaceParenthesis(copiedText)))).then((message) => {
                     resolve(message);
                 }).catch((err)=>{
                     reject(err);
@@ -18,6 +19,58 @@ export class ReplacementLib{
             });
         });
     }
+
+    static replaceAddOutParameter(text: string): string {
+        const lines = text.split('\n');
+        const replacedLines = lines.map((line) => {
+          if (line.includes('db.AddOutParameter(comando,')) {
+            const replacedLine = line.replace('db.AddOutParameter(comando,', 'comando.Parameters.Add(');
+            const valueIndex = replacedLine.indexOf(').Value');
+            const beforeValue = replacedLine.slice(0, valueIndex);
+            const afterValue = replacedLine.slice(valueIndex);
+            return `${beforeValue},ParameterDirection.Output${afterValue}`;
+          }
+          return line;
+        });
+        return replacedLines.join('\n');
+      }
+
+    static replaceParenthesis(text: string): string {
+        var searchString=').Value='
+        const lines = text.split('\n');
+        const replacedLines = lines.map((line) => {
+          if (line.includes(searchString) && line.trim().endsWith(');')) {
+            const lastIndex = line.lastIndexOf(')');
+            const lineWithoutParenthesis = line.slice(0, lastIndex) + ';' + line.slice(lastIndex + 1, -2);
+            return lineWithoutParenthesis;
+          }
+          return line;
+        });
+        return replacedLines.join('\n');
+    }
+    static replaceOracleDbType(text: string): string {
+        const lines = text.split('\n');
+        const replacedLines = lines.map((line) => {
+          if (line.includes('OracleDbType.Boolean')) {
+            const isTernary = line.includes('? true : false');
+            const replacedLine = line.replace('OracleDbType.Boolean', 'OracleDbType.Int32');
+            if (isTernary) {
+              const ternaryIndex = replacedLine.lastIndexOf('? true : false');
+              const beforeTernary = replacedLine.slice(0, ternaryIndex);
+              const afterTernary = replacedLine.slice(ternaryIndex);
+              const newTernary = afterTernary.replace(/true/g, '1').replace(/false/g, '0');
+              return `${beforeTernary}${newTernary}`;
+            } else {
+              const semicolonIndex = replacedLine.lastIndexOf(';');
+              const beforeSemicolon = replacedLine.slice(0, semicolonIndex);
+              const afterSemicolon = replacedLine.slice(semicolonIndex);
+              return `${beforeSemicolon} ? 1 : 0${afterSemicolon}`;
+            }
+          }
+          return line;
+        });
+        return replacedLines.join('\n');
+      }      
     static replaceText(mainText:string, searchValue:string, replacementValue:string):string{
         const searchEscaped = searchValue.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
         const regex = new RegExp(searchEscaped, 'g');
